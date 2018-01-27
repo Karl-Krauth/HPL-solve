676e6747fbab7c947a81cfc426bc1206edb04f12
EC2 Default User <ec2-user@ip-172-31-15-146.us-west-2.compute.internal>
2018-01-27 02:02:17 +0000

    matrix init code

---
+++ numpywren/matrix_init.py
@@ -0,0 +1,14 @@
+import numpy as np
+from numpywren.matrix_init import local_numpy_init
+import sys
+N = int(sys.argv[1])
+NB = int(sys.argv[2])
+x = np.random.rand(N, N)
+x = x + x.T
+print(N, NB)
+mat = local_numpy_init(x, [NB, NB], n_jobs=36, symmetric=True, write_header=True)
+print("Uploaded A")
+mat2 = local_numpy_init(np.random.rand(N, 1000), [NB, NB], n_jobs=36, write_header=True)
+with open("key", "w") as f:
+    f.write(mat.key + '\n')
+    f.write(mat2.key + '\n')
