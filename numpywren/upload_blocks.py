5f8613821b6a9acf0a2aee97d523a547538ad980
EC2 Default User <ec2-user@ip-172-31-0-192.us-west-2.compute.internal>
2018-02-01 21:08:50 +0000

    added exp backoff to header write.

--- numpywren/upload_blocks.py
+++ numpywren/upload_blocks.py
@@ -5,14 +5,26 @@ import struct
 import time
 
 with open("key", "r") as f:
-    key = f.readline().strip() + ".out"
+    key = "test(" + f.readline().strip() + ")"
 with open("shape", "r") as f:
     N = int(f.readline().strip())
     NB = int(f.readline().strip())
 idx_0 = int(sys.argv[1])
 idx_1 = int(sys.argv[2])
 print("UPLOADING %d %d" % (idx_0, idx_1))
-mat = BigMatrix(key, shape=[N, N+1000], shard_sizes=[NB, NB], write_header=True)
+cont = True
+sleep_time = 0
+while cont:
+    try:
+        time.sleep(sleep_time)
+        mat = BigMatrix(key, shape=[N, N+1000], shard_sizes=[NB, NB], write_header=True)
+        cont = False
+    except Exception as e:
+        if sleep_time == 0:
+            sleep_time = 1
+        else:
+            sleep_time *= 2
+        print("%d %d failed sleeping for %d and retrying" % (idx_0, idx_1, sleep_time))
 with open("/dev/shm/%d_%d" % (idx_0, idx_1), "rb") as f:
     MB = struct.unpack('i', f.read(4))[0]
     NB = struct.unpack('i', f.read(4))[0]
