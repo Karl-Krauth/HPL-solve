0998aba50faef656b101f5c15af56bcd89cfcf83
EC2 Default User <ec2-user@ip-172-31-1-237.us-west-2.compute.internal>
2018-02-01 13:54:00 +0000

    added exp backoff to download

--- numpywren/download_blocks.py
+++ numpywren/download_blocks.py
@@ -1,6 +1,8 @@
 import sys
 from numpywren.matrix import BigMatrix, BigSymmetricMatrix
 import numpy as np
+import time
+
 
 with open("key", "r") as f:
     key = f.readline().strip()
@@ -19,20 +21,33 @@ with open("shape", "w") as f:
 start_idx = idx_1 * mat.shard_sizes[0]
 end_idx = (idx_1 + 1) * mat.shard_sizes[0]
 num_blocks = ((mat.shape[0] + (mat.shard_sizes[0] - 1)) // mat.shard_sizes[0])
-if end_idx <= mat.shape[0]:
-    blk = mat.get_block(idx_0, idx_1)
-elif start_idx >= mat.shape[0]:
-    blk = B.get_block(idx_0, idx_1 - num_blocks)
-    last_col_len = (mat.shape[1] + B.shape[1]) % mat.shard_sizes[1]
-    blk = blk[:, -last_col_len:]
-elif start_idx < mat.shape[0] and end_idx > mat.shape[0]:
-    blk = mat.get_block(idx_0, idx_1)
-    blk2 = B.get_block(idx_0, 0)
-    blk = np.append(blk, blk2, axis=1)
-    if blk.shape[1] > mat.shard_sizes[0]:
-        blk = blk[:, :mat.shard_sizes[0]]
-else:
-    # TODO ensure we account for aligning
-    assert(False) 
+retry = True
+sleep_time = 0
+while retry:
+    try:
+        time.sleep(sleep_time)
+        if end_idx <= mat.shape[0]:
+            blk = mat.get_block(idx_0, idx_1)
+        elif start_idx >= mat.shape[0]:
+            blk = B.get_block(idx_0, idx_1 - num_blocks)
+            last_col_len = (mat.shape[1] + B.shape[1]) % mat.shard_sizes[1]
+            blk = blk[:, -last_col_len:]
+        elif start_idx < mat.shape[0] and end_idx > mat.shape[0]:
+            blk = mat.get_block(idx_0, idx_1)
+            blk2 = B.get_block(idx_0, 0)
+            blk = np.append(blk, blk2, axis=1)
+            if blk.shape[1] > mat.shard_sizes[0]:
+                blk = blk[:, :mat.shard_sizes[0]]
+        else:
+            # TODO ensure we account for aligning
+            assert(False) 
+        retry = False
+    except Exception as e:
+        print(e)
+        if sleep_time == 0:
+            sleep_time = 1
+        else:
+            sleep_time *= 2
+        print("Retrying after %d" % (sleep_time))
 with open("/dev/shm/%d_%d" % (idx_0, idx_1), "wb") as f:
     f.write(blk.T.tobytes())
